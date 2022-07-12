cd11cf7e477bc9f87363ec07e008cc03c929a558
Iztok <iztok.berdnik@gmail.com>
2022-07-12 17:30:05 +0200

    chore(core): Remove app routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AllHoursCoreModule } from './core/all-hours-core.module';
-import { AppRoutingModule } from './app-routing.module';
+
+const APPLICATION_ROUTES: Routes = [
+  {
+    canActivate: [ApplicationConfigurationGuard],
+    loadChildren: () =>
+      import('src/app/features/lazy/application-public-shell').then(
+        (m) => m.ApplicationPublicShellModule
+      ),
+    path: '',
+  },
+  {
+    path: '**',
+    redirectTo: 'url-not-found',
+  },
+];
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ApplicationConfigurationGuard } from './core';
 @NgModule({
   declarations: [AppComponent],
-  imports: [AllHoursCoreModule.forRoot(), AppRoutingModule, BrowserModule],
+  imports: [
+    AllHoursCoreModule.forRoot(),
+    RouterModule.forRoot(APPLICATION_ROUTES),
+    BrowserModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
