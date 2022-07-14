229141149da0cd51822ec3a815b162aaa224235a
Iztok Berdnik <iztok.berdnik@rcg.si>
2022-07-14 14:38:34 +0200

    feat(users): WIP Initial commit

---
+++ src/app/features/lazy/users/containers/users-container/users-container.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-users-container',
+  templateUrl: './users-container.component.html',
+  styleUrls: ['./users-container.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class UsersContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
