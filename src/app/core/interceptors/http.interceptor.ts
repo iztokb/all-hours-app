e779d74f308671b968bad07100d3bedbee3dd9d7
Iztok <iztok.berdnik@gmail.com>
2022-07-16 05:52:40 +0200

    feat(core): Redirect to authentication when 401 is received

--- src/app/core/interceptors/http.interceptor.ts
+++ src/app/core/interceptors/http.interceptor.ts
@@ -6,11 +6,14 @@ import {
   HttpInterceptor,
   HttpErrorResponse,
 } from '@angular/common/http';
-import { catchError, Observable, throwError } from 'rxjs';
+import { catchError, Observable, throwError, of, tap, switchMap } from 'rxjs';
+import { IApplicationState } from '../models';
+import { Store } from '@ngrx/store';
+import { LogoutAuthenticatedIdentityAction } from '../store';
 
 @Injectable()
 export class ApplicationHttpInterceptor implements HttpInterceptor {
-  constructor() {}
+  constructor(private _store: Store<IApplicationState>) {}
 
   intercept(
     request: HttpRequest<unknown>,
@@ -20,7 +23,18 @@ export class ApplicationHttpInterceptor implements HttpInterceptor {
       // For now only catch error. In the future, different flows can be implemented here
       // E.g. 401 - redirect to authentication module; 404 - redirect to Not found module
       catchError((error: HttpErrorResponse) => {
-        return throwError(() => new Error(error.message));
+        return of(error).pipe(
+          tap((httpError: HttpErrorResponse) => {
+            if (httpError.status === 401) {
+              this._store.dispatch(LogoutAuthenticatedIdentityAction());
+            }
+
+            return httpError as HttpErrorResponse;
+          }),
+          switchMap((httpError: HttpErrorResponse) => {
+            return throwError(() => new Error(error.message));
+          })
+        );
       })
     );
   }
